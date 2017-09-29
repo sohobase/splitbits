11c32a68c0e6d7f3d71062462c2de622105ead96
@soyjavi <javi.jimenez.villar@gmail.com>
2017-09-29 09:47:06 +0700

    #set animation on <Header> by property

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,28 +6,27 @@ import { SHAPE, STYLE } from '../config';
 import Button from './Button';
 import styles from './Header.style';
 
-const DEFAULT_ANIMATION = 'bounceInDown';
-
-const renderButton = (props, delay) => (
-  <Animatable animation={DEFAULT_ANIMATION} delay={delay}>
+const renderButton = (props, animation) => (
+  <Animatable {...animation}>
     <Button {...props} raised style={styles.button} />
   </Animatable>
 );
 
-const Header = ({ buttonRight = {}, children, navigation, style, title }) => (
+const Header = ({ animation, buttonRight = {}, children, navigation, style, title }) => (
   <View style={[STYLE.ROW, styles.header, style]}>
-    { navigation && renderButton({ icon: 'arrowBack', onPress: () => navigation.goBack() }, 200) }
-    <Animatable animation={DEFAULT_ANIMATION} delay={300} style={styles.content}>
+    { navigation && renderButton({ icon: 'arrowBack', onPress: () => navigation.goBack() }, { animation, delay: 200 }) }
+    <Animatable animation={animation} delay={300} style={styles.content}>
       <View style={STYLE.CENTERED}>
         { title && <Text style={styles.title}>{title}</Text> }
         { children }
       </View>
     </Animatable>
-    { (navigation || buttonRight.icon) && renderButton(buttonRight, 400) }
+    { (navigation || buttonRight.icon) && renderButton(buttonRight, { animation, delay: 400 }) }
   </View>
 );
 
 Header.propTypes = {
+  animation: string,
   buttonRight: SHAPE.BUTTON,
   children: oneOfType(array, node),
   navigation: SHAPE.NAVIGATION,
@@ -36,6 +35,7 @@ Header.propTypes = {
 };
 
 Header.defaultProps = {
+  animation: 'bounceInDown',
   buttonRight: undefined,
   navigation: undefined,
   children: undefined,
