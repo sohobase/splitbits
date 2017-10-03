c7d43ea462bd81418efa139d13ba24ba80b3254c
Mikel Garcia <mikel_6@hotmail.com>
2017-10-03 12:29:02 +0200

    Remove redux-persist-transform-encrypt

--- src/store/initialize.js
+++ src/store/initialize.js
@@ -1,15 +1,9 @@
 import { AsyncStorage } from 'react-native';
 import { compose, createStore } from 'redux';
 import { autoRehydrate, persistStore } from 'redux-persist';
-import createEncryptor from 'redux-persist-transform-encrypt';
 import reducer from './reducer';
 
-const { Constants: { deviceId = 'unknown' } } = Expo || {};// eslint-disable-line
-
 export default () => {
-  const encryptor = createEncryptor({
-    secretKey: deviceId,
-  });
   return new Promise((resolve) => {
     const store = createStore(
       reducer,
@@ -23,9 +17,6 @@ export default () => {
       store,
       {
         storage: AsyncStorage,
-        transforms: [
-          encryptor,
-        ],
       },
       () => resolve(store),
     );
