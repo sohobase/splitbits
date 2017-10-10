b847423f52269fb64ff8bfe60ba7226bc8280075
@soyjavi <javi.jimenez.villar@gmail.com>
2017-10-10 11:32:20 +0700

    Show activity in <Footer> if device has requests to attend.

--- src/screens/Main/components/Footer.js
+++ src/screens/Main/components/Footer.js
@@ -1,23 +1,37 @@
 import { func } from 'prop-types';
 import React from 'react';
 import { View } from 'react-native';
+import { connect } from 'react-redux';
 import { Option } from '../../../components';
-import { STYLE } from '../../../config';
+import { SHAPE, STYLE } from '../../../config';
 import styles from './Footer.style';
 
-const Footer = ({ navigate }) => (
+const Footer = ({ device: { requests = [] }, navigate }) => (
   <View style={[STYLE.ROW, STYLE.CENTERED, styles.footer]}>
-    <Option activity centered icon="profile" caption="Profile" onPress={() => navigate('Profile')} style={styles.option} />
+    <Option
+      activity={requests.length > 0}
+      centered
+      icon="profile"
+      caption="Profile"
+      onPress={() => navigate('Profile')}
+      style={styles.option}
+    />
     <Option centered icon="settings" caption="Settings" onPress={() => navigate('Settings')} style={styles.option} />
   </View>
 );
 
 Footer.propTypes = {
+  device: SHAPE.DEVICE,
   navigate: func,
 };
 
 Footer.defaultProps = {
+  device: {},
   navigate: undefined,
 };
 
-export default Footer;
+const mapStateToProps = ({ device }) => ({
+  device,
+});
+
+export default connect(mapStateToProps)(Footer);
