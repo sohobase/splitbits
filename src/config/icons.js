854f4668d5b8779541b6ce48d4ff2926dcd5da6a
@soyjavi <javi.jimenez.villar@gmail.com>
2017-11-16 10:08:13 +0700

    New icons

--- src/config/icons.js
+++ src/config/icons.js
@@ -1,26 +1,34 @@
 const add = require('../../assets/icon-add.png');
+const addDevice = require('../../assets/icon-add-device.png');
 const arrowBack = require('../../assets/icon-arrow-back.png');
 const arrowForward = require('../../assets/icon-arrow-forward.png');
+const backup = require('../../assets/icon-backup.png');
 const camera = require('../../assets/icon-camera.png');
 const check = require('../../assets/icon-check.png');
 const close = require('../../assets/icon-close.png');
 const create = require('../../assets/icon-create.png');
 const fingerprint = require('../../assets/icon-fingerprint.png');
 const gallery = require('../../assets/icon-gallery.png');
+const menu = require('../../assets/icon-menu.png');
 const operations = require('../../assets/icon-operations.png');
 const profile = require('../../assets/icon-profile.png');
+const remove = require('../../assets/icon-remove.png');
 const search = require('../../assets/icon-search.png');
 const security = require('../../assets/icon-security.png');
 const settings = require('../../assets/icon-settings.png');
 const split = require('../../assets/icon-split.png');
+const star = require('../../assets/icon-star.png');
 const swap = require('../../assets/icon-swap.png');
 const trendingDown = require('../../assets/icon-trending-down.png');
 const trendingUp = require('../../assets/icon-trending-up.png');
+const verified = require('../../assets/icon-verified.png');
 
 export default {
   add,
+  addDevice,
   arrowBack,
   arrowForward,
+  backup,
   camera,
   check,
   close,
@@ -28,13 +36,17 @@ export default {
   fingerprint,
   gallery,
   import: add,
+  menu,
   operations,
   profile,
+  remove,
   search,
   security,
   settings,
   split,
+  star,
   swap,
   trendingDown,
   trendingUp,
+  verified,
 };
