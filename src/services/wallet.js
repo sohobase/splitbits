098901d046bf5f3858063f62d22136e07c3006ca
David Bengoa <david@bengoarocandio.com>
2017-11-16 13:22:38 +0700

    Less hacks, move hacks to their own dir

--- src/services/wallet.js
+++ src/services/wallet.js
@@ -1,34 +1,17 @@
+import './node-hacks'
+import bitcoinjs from 'bitcoinjs-lib'
 import { PushService } from './';
 import { service } from './modules';
-
-import { Buffer } from 'buffer'
-global.Buffer = Buffer; // node modules expect Buffer to be available as a global
-process.browser = true;
-import BigInteger from 'bigi'
-const bitcoinjs = require('bitcoinjs-lib');
-import ecurve from 'ecurve';
-const secp256k1 = ecurve.getCurveByName('secp256k1');
 
 const networks = {
   BTC: bitcoinjs.networks.bitcoin,
   LTC: bitcoinjs.networks.litecoin
 };
 
-const randomPrivateKey = () => {
-  do {
-    let b = Buffer.alloc(32);
-    for (let i = 0; i < 32; i++) { // TODO: use a CSPRNG
-      b[i] = Math.floor(Math.random() * 256);
-    }
-    var d = BigInteger.fromBuffer(b);
-  } while(d.signum() <= 0 || d.compareTo(secp256k1.n) >= 0)
-  return d;
-};
 
 export default {
-
   create(props) {
-    const ecPair = new bitcoinjs.ECPair(randomPrivateKey(), null, { network:  networks[props.coin]});
+    const ecPair = new bitcoinjs.ECPair.makeRandom({ network:  networks[props.coin] });
     return this.import({ ...props, wif: ecPair.toWIF() })
   },
 
