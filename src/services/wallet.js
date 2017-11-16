742f5d51a5e7f2e27b099150545467a60dc8206f
David Bengoa <david@bengoarocandio.com>
2017-11-16 13:48:28 +0700

    Send imported property to the client

--- src/services/wallet.js
+++ src/services/wallet.js
@@ -2,11 +2,11 @@ import './node-hacks'
 import bitcoinjs from 'bitcoinjs-lib'
 import { PushService } from './';
 import { service } from './modules';
-import { CRYPTO_NAMES } from '../config/constants'
+import { C } from '../config'
 
 export default {
   create(props) {
-    const network = bitcoinjs.networks[CRYPTO_NAMES[props.network]];
+    const network = bitcoinjs.networks[C.CRYPTO_NAMES[props.coin]];
     const ecPair = new bitcoinjs.ECPair.makeRandom({ network });
     return this._save({ ...props, imported: false, wif: ecPair.toWIF() });
   },
@@ -16,9 +16,12 @@ export default {
   },
 
   async _save(props) {
-    const { coin, name, wif } = props;
+    const { coin, name, wif, imported } = props;
     const address = bitcoinjs.ECPair.fromWIF(wif).getAddress()
-    const resp = await service('wallet', { method: 'POST', body: JSON.stringify({coin, name, address}) });
+    const resp = await service('wallet', {
+      method: 'POST',
+      body: JSON.stringify({coin, name, address, imported})
+    });
     return { ...resp, wif }
   }
 };
