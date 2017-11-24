29b051c24b2259b22e079e367fa1dd4e94d027ea
David Bengoa <david@bengoarocandio.com>
2017-11-24 11:32:13 +0700

    Fix logo asset

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Image } from 'react-native';
 import styles from './Logo.style';
 
-const asset = require('../../assets/app-logo.png');
+const asset = require('../../assets/app-brandname.png');
 
 const Logo = ({ style }) => <Image style={[styles.logo, style]} source={asset} />;
 
