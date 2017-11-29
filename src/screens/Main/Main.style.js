c6f8d58820bf451e3ade651076503e2db5b80709
David Bengoa <david@bengoarocandio.com>
2017-11-29 11:18:01 +0700

    Fix wallets style in android

--- src/screens/Main/Main.style.js
+++ src/screens/Main/Main.style.js
@@ -6,7 +6,6 @@ const { OFFSET, WALLET_HEIGHT } = THEME;
 export default StyleSheet.create({
   wallets: {
     height: WALLET_HEIGHT + (OFFSET * 4),
-    alignSelf: 'flex-end',
   },
 
   pagination: {
