45e24398fb39b69c19d64a6cfdf0ca0641783fec
@soyjavi <javi.jimenez.villar@gmail.com>
2017-12-04 12:27:50 +0700

    #refacto SOLID

--- src/screens/Main/modules/onNotification.js
+++ src/screens/Main/modules/onNotification.js
@@ -1,24 +1,21 @@
 import { DeviceService, TransactionService, WalletService } from '../../../services';
 import { updateDeviceAction, updateTransactionsAction, updateWalletAction } from '../../../store/actions';
 
-export default async({
-  origin, title, body, data = {},
-}) => {
-  const { device, wallet: walletId } = data;
+export default async({ data = {} }) => {
+  const { device, wallet } = data;
   let response;
-  console.log('{PUSH}', origin, title, body, origin, { device, walletId });
 
   if (device) {
     response = await DeviceService.state();
     if (response) updateDeviceAction(response);
   }
 
-  if (walletId) {
-    const [wallet, transactions] = Promise.all([
-      WalletService.state(walletId),
-      TransactionService.list(walletId),
+  if (wallet) {
+    const values = Promise.all([
+      WalletService.state(wallet),
+      TransactionService.list(wallet),
     ]);
-    if (wallet) updateWalletAction(wallet);
-    if (transactions) updateTransactionsAction(transactions);
+    if (values[0]) updateWalletAction(values[0]);
+    if (values[1]) updateTransactionsAction(values[1]);
   }
 };
