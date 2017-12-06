62fc33b2b0df6ee8402a1756129ce1bcdbe5cb85
@soyjavi <javi.jimenez.villar@gmail.com>
2017-12-06 18:22:09 +0700

    #bugfix limit inactivity in background of 1 minute.

--- src/screens/Main/modules/onAppActive.js
+++ src/screens/Main/modules/onAppActive.js
@@ -2,12 +2,20 @@ import { NavigationActions } from 'react-navigation';
 import { C } from '../../../config';
 
 const { STATE: { ACTIVE } } = C;
+const LIMIT_SECONDS_IN_BACKGROUND = 60;
+let lastActivity;
 
 export default ({ navigation }, state) => {
+  if (!lastActivity) lastActivity = new Date();
+
   if (state === ACTIVE) {
-    navigation.dispatch(NavigationActions.reset({
-      index: 0,
-      actions: [NavigationActions.navigate({ routeName: 'Lock' })],
-    }));
+    const seconds = parseInt((new Date() - lastActivity) / 1000, 10);
+    if (seconds > LIMIT_SECONDS_IN_BACKGROUND) {
+      navigation.dispatch(NavigationActions.reset({
+        index: 0,
+        actions: [NavigationActions.navigate({ routeName: 'Lock' })],
+      }));
+    }
+    lastActivity = 0;
   }
 };
