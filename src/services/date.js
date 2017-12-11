93a134e0972f07634f5c4c56396ce6e0dbfe41fe
@soyjavi <javi.jimenez.villar@gmail.com>
2017-12-11 15:47:33 +0700

    #improvement use intl object for get a better date expression

--- src/services/date.js
+++ src/services/date.js
@@ -1,11 +1,24 @@
 import timeago from 'timeago.js';
 
+const { DateTimeFormat } = Intl;
+const options = {
+  weekday: 'short',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+  timeZone: 'UTC',
+  timeZoneName: 'short',
+};
+
 export default {
   ago(date) {
     return timeago().format(date);
   },
 
-  locale(date, encode = 'en-us') {
-    return new Date(date).toLocaleString(encode);
+  locale(date, encode = 'en-US') {
+    return DateTimeFormat(encode, options).format(new Date(date));
   },
 };
