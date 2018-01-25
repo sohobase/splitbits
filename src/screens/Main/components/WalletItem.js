fca2efbcac6e7bbc1d30bc2ef615dde217270b8f
@soyjavi <javi.jimenez.villar@gmail.com>
2018-01-25 11:00:41 +0700

    #improvement show date as MM/YY using the first transaction of a determinate wallet

--- src/screens/Main/components/WalletItem.js
+++ src/screens/Main/components/WalletItem.js
@@ -6,11 +6,21 @@ import { connect } from 'react-redux';
 
 import { ASSETS, C, SHAPE, STYLE, THEME } from '../../../config';
 import { Amount, Button, Touchable } from '../../../components';
+import { walletTransactions } from '../modules';
 import styles from './WalletItem.style';
 
 const { SATOSHI, TYPE } = C;
 const { ANIMATION: { DURATION } } = THEME;
 const NEW_WALLET_OPTIONS = ['CREATE', 'IMPORT', 'RECOVER'];
+
+const MMYY = (value) => {
+  const date = value ? new Date(value) : new Date();
+  let MM = date.getMonth() + 1;
+  if (MM < 10) MM = `0${MM}`;
+  const YY = date.getUTCFullYear().toString().substring(2);
+
+  return `${MM}/${YY}`;
+};
 
 const WalletItem = ({
   currencies,
@@ -18,6 +28,7 @@ const WalletItem = ({
     address, balance = 0, coin, id, name = '', readOnly, type,
   } = {},
   device: { currency },
+  firstTransaction: { createdAt } = {},
   i18n,
   onPress,
   style,
@@ -66,7 +77,7 @@ const WalletItem = ({
                 <Text style={[styleText, styles.typeWriter]}>{address.slice(-4).toUpperCase()}</Text>
               </View>
               <View style={STYLE.ROW}>
-                <Text style={[styleText, styles.typeWriter, styles.date]}>??/??</Text>
+                <Text style={[styleText, styles.typeWriter, styles.date]}>{MMYY(createdAt)}</Text>
                 <Image source={ASSETS[coin]} style={styles.coinLogo} />
               </View>
             </View>
@@ -98,6 +109,7 @@ WalletItem.propTypes = {
   currencies: shape(SHAPE.CURRENCIES),
   data: shape(SHAPE.WALLET).isRequired,
   device: shape(SHAPE.DEVICE).isRequired,
+  firstTransaction: shape(SHAPE.TRANSACTION),
   i18n: shape(SHAPE.I18N).isRequired,
   onPress: func,
   style: oneOfType([array, number]),
@@ -105,14 +117,22 @@ WalletItem.propTypes = {
 
 WalletItem.defaultProps = {
   currencies: {},
+  firstTransaction: undefined,
   onPress: undefined,
   style: [],
 };
 
-const mapStateToProps = ({ currencies, device, i18n }) => ({
-  currencies: currencies[device.currency],
-  device,
-  i18n,
-});
+const mapStateToProps = ({
+  currencies, device, i18n, transactions,
+}, { data: wallet }) => {
+  const storeTransactions = walletTransactions(wallet, transactions) || [];
+
+  return ({
+    currencies: currencies[device.currency],
+    device,
+    firstTransaction: storeTransactions[storeTransactions.length - 1],
+    i18n,
+  });
+};
 
 export default connect(mapStateToProps)(WalletItem);
