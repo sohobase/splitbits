926d82a48103ca92493d57c60874fa400cd509f9
@soyjavi <javi.jimenez.villar@gmail.com>
2018-02-07 12:31:12 +0700

    #fixed typo

--- src/components/Touchable.js
+++ src/components/Touchable.js
@@ -1,10 +1,22 @@
+import { bool } from 'prop-types';
 import React from 'react';
 import { Platform, TouchableHighlight, TouchableNativeFeedback, TouchableOpacity } from 'react-native';
 
-export default ({ raised, ...props }) => { //eslint-disable-line
+
+const Touchable = ({ raised, ...props }) => {
   if (raised) return <TouchableOpacity {...props} />;
 
   return Platform.OS === 'ios'
     ? <TouchableHighlight underlayColor="rgba(0,0,0,0.15)" activeOpacity={0.85} {...props} />
     : <TouchableNativeFeedback useForeground {...props} />;
 };
+
+Touchable.propTypes = {
+  raised: bool,
+};
+
+Touchable.defaultProps = {
+  raised: false,
+};
+
+export default Touchable;
